1c852c0e81e9c9cf1435c2a20a8cdcdd2f46d283
simonegrimaldi <simonegrimaldi84@gmail.com>
2025-06-06 09:22:55 +0200

    ultima view

---
+++ src/controller/QuestionController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author simonegrimaldi
+ */
+public class QuestionController implements Initializable {
+
+    @FXML
+    private CheckBox checkBoxA;
+    @FXML
+    private CheckBox checkBoxB;
+    @FXML
+    private CheckBox checkBoxC;
+    @FXML
+    private Button confirmButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void confirmButtonClick(ActionEvent event) {
+    }
+    
+}
