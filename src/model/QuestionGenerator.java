6d7627a902547230d5682147538cafe9912ab5b2
corry <corry@localhost>
2025-06-06 15:58:21 +0200

    Classe Question(Generator?)

---
+++ src/model/QuestionGenerator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author corry
+ */
+public class Question {
+    private Analysis analysis;
+    private String question;
+    private int answer;
+    
+    public QuestionGenerator(Analysis analysis){
+        this.analysis = analysis;
+    }
+    public String createQuestion(int type){
+        
+    
+}
