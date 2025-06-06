694e9ae2119df09f4794f31b62f4b36cfc5971ea
Michele Cetraro <michelecetraro9904@gmail.com>
2025-06-06 15:50:23 +0200

    - Aggiunta package interfaces - Creazione interface IOFile per gestire il caricamento dei file - Creazione e implementazione della classe IOFileClass che gestisce   il caricameno dei file

---
+++ src/Interfaces/IOFile.java
@@ -0,0 +1,21 @@
+package Interfaces;
+
+/**
+ * @interface IOFile
+ *
+ * Questa interfaccia definisce i metodi necessari per caricare e salvare file.
+ * Le classi che implementano questa interfaccia devono fornire
+ * un'implementazione concreta per il caricamento e il salvataggio dei file,
+ * come lettura da un file di testo o la gestione di file binari.
+ */
+public interface IOFile {
+
+    /**
+     * Carica il contenuto di un file
+     *
+     * @param filename, il nome del file da caricare
+     * @return Il contenuto del file selezionato in formato String
+     */
+    public String loadFile(String filename);
+
+}
---
+++ src/Interfaces/IOFileClass.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+/**
+ * Carica il contenuto di un file di testo.
+ *
+ * Questo metodo apre un file di testo, legge ogni sua riga e restituisce il
+ * contenuto come una stringa. Se il file non viene trovato, restituisce un
+ * messaggio di errore.
+ *
+ * @param filename Il nome del file da caricare (senza estensione .txt).
+ * @return Il contenuto del file come una stringa, o un messaggio di errore se
+ * il file non è stato trovato.
+ * @throws FileNotFoundException Se il file non viene trovato nel percorso
+ * specificato.
+ */
+public class IOFileClass implements IOFile {
+
+    @Override
+    public String loadFile(String filename) {
+        String contenutoFile = null;
+        try (Scanner scanner = new Scanner(new FileReader(filename + ".txt"))) {
+            StringBuilder sb = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                sb.append(scanner.nextLine()).append("\n");
+            }
+            contenutoFile = sb.toString();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            System.err.println("ERRORE IN LOADFILE");
+        }
+        return contenutoFile;
+    }
+}
