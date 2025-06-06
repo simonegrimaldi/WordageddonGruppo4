fbf390962f49bb9543922437690b610feb645352
Pas1267 <p.rodia@studenti.unisa.it>
2025-06-06 21:48:48 +0200

    aggiunta signup controller

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -4,6 +4,7 @@
  */
 package controller;
 
+import dao.implementation.DaoUserImpl;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -27,21 +28,42 @@ public class SignUpController implements Initializable {
     private Button backButton;
     @FXML
     private Button SignUpButton;
-
+    private DaoUserImpl dao;
     /**
      * Initializes the controller class.
      */
+    ChangeView controller;
+    AlertManager alert;
+      
+    public void setChangeViewController(ChangeView controller) {
+        this.controller = controller;
+    }
+
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        dao=new DaoUserImpl();
+        alert=new AlertManager();
     }    
 
     @FXML
     private void backButtonClick(ActionEvent event) {
+        controller.goLogIn();
     }
 
     @FXML
     private void SignUpButtonClick(ActionEvent event) {
+        String username=usernameField.getText();
+        String password=passwordField.getText();
+        if(username.isEmpty() || password.isEmpty()){
+            alert.showAlert("Errore", "Tutti i campi sono obbligatori");
+        }else{
+        if(dao.registration(username, password)){
+            controller.goHome(username);
+        }else{
+            alert.showAlert("Errore", "L'username già esiste");
+        }
+        }
     }
     
 }
