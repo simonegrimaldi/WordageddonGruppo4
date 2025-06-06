6204d37f74a4bcc261ffd4238bcc93227ec9a10c
Michele Cetraro <michelecetraro9904@gmail.com>
2025-06-06 15:52:16 +0200

    Aggiunta della IOFileClass nel package corretto

--- src/model/IOFileClass.java
+++ src/model/IOFileClass.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Interfaces;
+package model;
 
+import Interfaces.IOFile;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
