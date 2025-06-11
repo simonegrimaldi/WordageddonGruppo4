525e79d0ac1f10229db050f09932d9275fe54e51
Michele Cetraro <michelecetraro9904@gmail.com>
2025-06-11 10:27:21 +0200

    Added doxygen documentation for IOTexts interface

--- src/IOOperation/IOTexts.java
+++ src/IOOperation/IOTexts.java
@@ -2,12 +2,30 @@ package IOOperation;
 
 import java.util.List;
 
+/**
+ * @interface IOTexts
+ *
+ * @brief Interfaccia per la gestione delle operazioni di recupero dei testi.
+ *
+ * L'interfaccia definisce un metodo per cercare e restituire una lista di testi
+ * associati ad una specifica difficoltà (facile, media, difficile). La scelta
+ * dei testi all'interno della cartella corrispondende alla difficoltà sarà
+ * completamente casuale
+ */
 public interface IOTexts {
+
     /**
-     * Restituisce una lista di testi scelti casualmente dalla cartella associata alla difficoltà.
-     * - "facile": 1 testo
-     * - "media": 2 testi
-     * - "difficile": 3 testi
+     * @brief Restituisce una lista di testi scelti casualmente dalla cartella
+     * associata alla difficoltà. Il numero di testi scelti dipende dalla
+     * difficoltà: - "facile": 1 testo - "media": 2 testi - "difficile": 3 testi
+     *
+     * I testi verranno letti dalla cartella corrispondente alla difficoltà e
+     * restituiti sotto forma di stringa concatenata.
+     *
+     * @param difficolta la difficoltà secondo la quale recuperare i
+     * testi(facile, medio, difficile)
+     * @return Una lista di stringhe che contiene i percorsi associati ai file
+     * che sono stati scelti.
      */
     public List<String> cercaTesti(String difficolta);
 }
