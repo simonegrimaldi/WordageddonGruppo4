0581bdb3b05b2d4a25fbf608e01edb03f9ca79a1
Michele Cetraro <michelecetraro9904@gmail.com>
2025-06-12 09:57:45 +0200

    Added doxygen documentation for AlertManager class

--- src/util/AlertManager.java
+++ src/util/AlertManager.java
@@ -1,28 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package util;
 
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 
 /**
- * Gestione degli alert in modo centralizzato.
+ * @class AlertManager
+ * @brief Classe che gestisce gli alert in modo centralizzato all'interno
+ * di tutta l'applicazione
+ * 
+ * La classe fornisce un metodo per mostrare alert di tipo diverso (di errore,
+ * di conferma) in modo centralizzato. Gestisce il titolo, il contenuto e la 
+ * tipologia di alert. 
+ * 
  */
 public class AlertManager {
 
     /**
-     * Mostra un alert di tipo specificato.
+     * @brief Mostra un alert di tipo specificato
+     * 
+     * Il metodo crea un alert con il tipo e il titolo specificato. 
+     * Nel caso in cui l'alert sia di tipo 'CONFIRMATION', l'utente dovrà rispondere
+     * tramite un pulsante ('ok' o 'confirm'); tutti gli altri tipi di alert vengono
+     * mostrati senza aspettarsi una risposta
      * 
      * @param title Titolo dell'alert
      * @param message Messaggio dell'alert
      * @param type Tipo dell'alert (es. "CONFIRMATION", "ERROR", ecc.)
-     * @return La risposta dell'utente sotto forma di ButtonType, se il tipo è una conferma
+     * @return La risposta dell'utente sotto forma di ButtonType,
+     * se il tipo 'CONFIRMATION'
+     * 
+     * Restituisce {@link ButtonType#CANCEL} se l'utente non seleziona nulla.
+     * Se il tipo non è "CONFIRMATION", non restituisce nulla.
+     * 
+     * @pre {@code type} deve essere un valore valido riconosciuto
+     * dalla classe {@link Alert.AlertType}.
+     * @post Mostra un alert all'utente e restituisce la risposta dell'utente
+     * (solo per gli alert di tipo "CONFIRMATION").
+
      */
     public ButtonType showAlert(String title, String message, String type) {
-        // Crea l'alert con il tipo specificato
+        // Creo l'alert con il tipo specificato
         Alert alert = new Alert(Alert.AlertType.valueOf(type.toUpperCase()));
 
         alert.setTitle(title);  
@@ -30,10 +47,8 @@ public class AlertManager {
         alert.setContentText(message);
         
         if (type.equalsIgnoreCase("CONFIRMATION")) {
-            // Solo se è di tipo CONFIRMATION, mostriamo e aspettiamo una risposta
-            return alert.showAndWait().orElse(ButtonType.CANCEL);  // Restituisce ButtonType.OK o ButtonType.CANCEL
+            return alert.showAndWait().orElse(ButtonType.CANCEL);  
         } else {
-            // Per tipi che non richiedono risposta, mostriamo e chiudiamo senza restituire nulla
             alert.showAndWait();
             return null;
         }
